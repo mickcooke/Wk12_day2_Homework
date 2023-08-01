04dc8ac8b9b2fe57964943ea1e5836b285a59fb1
mickcooke <128602534+mickcooke@users.noreply.github.com>
2023-08-01 18:12:38 +0100

    Third Commit

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,8 +3,19 @@ import java.util.ArrayList;
 public class Library {
 
     private ArrayList<Book> stock;
+    private Integer capacity;
 
     public Library(){
         this.stock = new ArrayList<>();
+        this.capacity = 2600;
+
+    }
+
+    public int countStock() {
+        return this.stock.size();
+    }
+
+    public void addBook(Book book) {
+        this.stock.add(book);
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+     private Book book;
+
+     private Library library;
+
+     @Before
+    public void before() {
+//         book1 = new Book("Harry Potter And The Order of The Phoenix", "JK Rowling", "Childrens");
+//         book2 = new Book("Incognito: The Secret Lives of The Brain", "David Eagleman", "Science");
+         library = new Library();
+     }
+
+     @Test
+    public void libraryStartsEmpty() {
+         assertEquals(0, library.countStock());
+     }
+
+     @Test
+    public void canAddBook() {
+         Book book1 = new Book("Harry Potter And The Order of The Phoenix", "JK Rowling", "Childrens");
+         library.addBook(book1);
+         assertEquals(1, library.countStock());
+
+     }
+
+
+
 }
